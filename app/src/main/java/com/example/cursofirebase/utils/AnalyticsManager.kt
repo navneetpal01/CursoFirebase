e6bc2b6ecdd96d7345124c1ec5073c479c4cbdb7
navneet <navneetpalgithub@gmail.com>
2024-03-30 12:24:36 +0530

    28

---
+++ app/src/main/java/com/example/cursofirebase/utils/AnalyticsManager.kt
@@ -0,0 +1,53 @@
+package com.example.cursofirebase.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.logEvent
+
+
+class AnalyticsManager(
+    context: Context
+) {
+    //Multiple instances for each contexts
+    //lazy only initializes the value when accessed
+    private val analyticsManager: FirebaseAnalytics by lazy { FirebaseAnalytics.getInstance(context) }
+    //It retrieves the FirebaseAnalytics instance that's associated with that central Firebase configuration
+//    private val analyticsManager2 : FirebaseAnalytics by lazy { Firebase.analytics}
+
+
+    fun logButtonClicked(buttonName: String) {
+        val params = Bundle().apply {
+//          Putting a Key-Value Pair
+            putString("button_name", buttonName)
+        }
+        analyticsManager.logEvent("button_clicked", params)
+    }
+
+    @Composable
+    fun LogScreenView(screenName : String) {
+        DisposableEffect(key1 = Unit) {
+            onDispose {
+                val params = Bundle().apply {
+                    putString(FirebaseAnalytics.Param.SCREEN_NAME,screenName)
+                    putString(FirebaseAnalytics.Param.SCREEN_CLASS,screenName)
+                }
+                analyticsManager.logEvent(FirebaseAnalytics.Event.SCREEN_VIEW,params)
+            }
+        }
+
+    }
+
+    @SuppressLint("InvalidAnalyticsName")
+    fun logError(error : String){
+        val params = Bundle().apply {
+            putString("error",error)
+        }
+        analyticsManager.logEvent("error",params)
+    }
+
+
+}
